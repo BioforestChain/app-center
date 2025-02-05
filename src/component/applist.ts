e75a2fe77375962a3cf34f91c5c4a4e1c8660528
阿桂 <420288518@qq.com>
2025-02-05 18:51:27 +0800

    nilai应用分支

--- src/component/applist.ts
+++ src/component/applist.ts
@@ -5,9 +5,9 @@ import { type AppInfo, type ConfigData, type BaseConfig, type ProductSetByType }
 class AppList {
   // 获取应用的url地址
   private applistUrl_test: string =
-    'https://source.dwebdapp.com/dweb-browser-apps/dweb-apps-test/tansocc-list.json'
+    'https://source.dwebdapp.com/dweb-browser-apps/dweb-apps-test/nilai-list.json'
   private applistUrl_prod: string =
-    'https://source.dwebdapp.com/dweb-browser-apps/dweb-apps/tansocc-list.json'
+    'https://source.dwebdapp.com/dweb-browser-apps/dweb-apps/nilai-list.json'
 
   private data: ConfigData | undefined
 
